2ca774732d893778e77b4d28619648f28d98ae61
Thibaut DUMOULIN <tdumouli@e2r4p7.42.fr>
2017-02-12 19:14:16 +0100

    segfault

--- ls.h
+++ ls.h
@@ -6,13 +6,27 @@
 /*   By: tdumouli <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/22 18:30:55 by tdumouli          #+#    #+#             */
-/*   Updated: 2017/01/29 22:15:12 by tdumouli         ###   ########.fr       */
+/*   Updated: 2017/02/11 23:01:12 by tdumouli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LS_H
 # define LS_H
 # include <string.h>
+# include <sys/stat.h>
+
+typedef struct	s_flag_ls
+{
+	char			mr;
+	char			r;
+	char			a;
+	char			l;
+	char			t;
+}				t_flag_ls;
+
+t_flag_ls		*init_flag_ls(void);
+int				def(t_flag_ls *f, char *s);
+void			ft_flag_lstdelone(t_flag_ls **adlst);
 
 typedef struct	s_slist
 {
@@ -21,10 +35,18 @@ typedef struct	s_slist
 	struct s_slist	*next;
 }				t_slist;
 
-int 	ft_slstadd(t_slist **q, const void *i, const void *c);
-void	ft_slstdelone(t_slist **q);
-void	ft_slstdel(t_slist **q);
-t_slist	*ft_slstreverse(t_slist **q);
-int		affiche(char *c);
+int				ft_slstadd(t_slist **q, void *i, void *c, t_slist *f);
+void			ft_slstdelone(t_slist **q);
+void			ft_slstdel(t_slist **q);
+t_slist			*ft_slstreverse(t_slist **q);
+void			*ft_slstsort(t_slist **adlst, int (*f)(void *, void *));
+
+void			option_l(struct stat a);
+int				total(t_slist *ite);
+int				dossier(char *c, t_flag_ls *f);
+int				fichier(char *dir, t_flag_ls *f, t_slist **t, t_slist *i);
+void			recu(t_slist *rec, t_flag_ls *flag);
+int				morething(char **av, t_flag_ls *flag, int i, int ac);
+int				onething(char *av, t_flag_ls *flag);
 
 #endif
